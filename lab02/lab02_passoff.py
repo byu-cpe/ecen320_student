4ce2ebb6f807f5e3c7a6b10ef5839ec99065eb77
wirthlin <wirthlin@byu.edu>
2024-12-28 08:14:42 -0700

    Updated lab 2 passoff

--- lab02/lab02_passoff.py
+++ lab02/lab02_passoff.py
@@ -13,15 +13,10 @@ import test_suite_320
 import repo_test
 
 def main():
-    tester = test_suite_320.build_test_suite_320("lab02", max_repo_files = 15)
-    # Add a test that checks to make sure the vivado tools are available in the executing environment
-    #  (check_exec_test)
-    # tester.add_make_test("about")
-    # tester.add_make_test("reverse")
-    # tester.add_make_test("upper")
-    # tester.add_make_test("lineno")
-    # tester.add_make_test("bottom")
-    # tester.add_file_checks(["reverse.txt", "upper.txt", "upper.txt", "lineno.txt", "bottom.txt"])
+    tester = test_suite_320.build_test_suite_320("lab02", start_date="01/20/2025")
+    tester.add_Makefile_rule("sim_adder", ["sim_adder.tcl"], ["sim_adder.log"])
+    tester.add_Makefile_rule("bottom", ["aboutme.txt"], ["bottom.txt"])
+    tester.add_required_tracked_files(["netid.jpg"])
     tester.run_tests()
 
 if __name__ == "__main__":
