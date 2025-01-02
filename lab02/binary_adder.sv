c947ac9fe27fa856926a62dd79205537fa52bd4f
wirthlin <wirthlin@byu.edu>
2025-01-02 14:57:26 -0700

    Changed binary adder back to 2 bits

--- lab02/binary_adder.sv
+++ lab02/binary_adder.sv
@@ -1,47 +1,29 @@
 // binary_adder.sv
 //
-// Perform binary addition on two 4-bit signals to produce a 5-bit output.
+// Perform binary addition on two 2-bit signals to produce a 3-bit output.
 //
-// switches sw[3:0] are the first four-bit binary input
-// switches sw[7:4] are the second four-bit binary input
+// switches sw[1:0] are the first two-bit binary input
+// switches sw[3:2] are the second two-bit binary input
 //
-// The result is a 5-bit number that is displayed on the LEDs led[4:0]
+// The result is a 3-bit number that is displayed on the LEDs led[2:0]
 
 
 module binary_adder (
-        input logic [7:0] sw,   // First four switches for input
-        output logic [4:0] led  // First three LEDs for output
+        input logic [3:0] sw,   // First four switches for input
+        output logic [2:0] led  // First three LEDs for output
     );
-    logic [7:0] carry;          // Carry of the two inputs
-    logic [7:0] carry_gen;      // Carry generate
-    logic [7:0] carry_prop;     // Carry propagate
-    logic [7:0] carry_in_prop;  // Carry in propagate
+    logic [2:0] carry;          // Carry signals
 
     // Bit 0
-    xor(led[0], sw[0], sw[4]);                      // sum[0]
-    and(carry_gen[0], sw[0], sw[4]);                // carry_gen[0]
-    or(carry_prop[0], sw[0], sw[4]);                // carry_prop[0]
-    and(carry_in_prop[0], carry_prop[0], 1'b0);     // No carry in for bit 0
-    or(carry[0], carry_gen[0], carry_in_prop[0]);   // carry[0]
+    xor(led[0], sw[0], sw[2]);                      // sum[0]
+    and(carry[0], sw[0], sw[2]);                    // carry[0]
     // Bit 1
-    xor(led[1], sw[1], sw[5]);                      // sum[1]
-    and(carry_gen[1], sw[1], sw[5]);                // carry_gen[1]
-    or(carry_prop[1], sw[1], sw[5]);                // carry_prop[1]
-    and(carry_in_prop[1], carry_prop[1], carry[0]);
-    or(carry[1], carry_gen[1], carry_in_prop[1]);   // carry[1]
-    // Bit 2
-    xor(led[2], sw[2], sw[6]);                      // sum[2]
-    and(carry_gen[2], sw[2], sw[6]);                // carry_gen[2]
-    or(carry_prop[2], sw[2], sw[6]);                // carry_prop[2]
-    and(carry_in_prop[2], carry_prop[2], carry[1]);
-    or(carry[2], carry_gen[2], carry_in_prop[2]);   // carry[2]
+    xor(led[1], sw[1], sw[3]);                      // sum[1]
+    and(a_and_b, sw[1], sw[3]);
+    and(a_and_c, sw[1], carry[0]);
+    and(b_and_c, sw[3], carry[0]);
+    or(carry[1], a_and_b, a_and_c, b_and_c);        // carry[1]
     // Bit 3
-    xor(led[3], sw[3], sw[7]);                      // sum[3]
-    and(carry_gen[3], sw[3], sw[7]);                // carry_gen[3]
-    or(carry_prop[3], sw[3], sw[7]);                // carry_prop[3]
-    and(carry_in_prop[3], carry_prop[3], carry[2]);
-    or(carry[3], carry_gen[3], carry_in_prop[3]);   // carry[3]
-    // Bit 4 (carry out from carry[3])
-    buf(led[4], carry[3]);                  
+    buf(led[2], carry[1]);                  
 
 endmodule
--- lab02/binary_adder.xdc
+++ lab02/binary_adder.xdc
@@ -9,14 +9,7 @@ set_property PACKAGE_PIN W16 [get_ports {sw[2]}]
 	set_property IOSTANDARD LVCMOS33 [get_ports {sw[2]}]
 set_property PACKAGE_PIN W17 [get_ports {sw[3]}]
 	set_property IOSTANDARD LVCMOS33 [get_ports {sw[3]}]
-set_property PACKAGE_PIN W15 [get_ports {sw[4]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[4]}]
-set_property PACKAGE_PIN V15 [get_ports {sw[5]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[5]}]
-set_property PACKAGE_PIN W14 [get_ports {sw[6]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[6]}]
-set_property PACKAGE_PIN W13 [get_ports {sw[7]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[7]}]
+
 
 ## LEDs
 set_property PACKAGE_PIN U16 [get_ports {led[0]}]
@@ -25,10 +18,6 @@ set_property PACKAGE_PIN E19 [get_ports {led[1]}]
 	set_property IOSTANDARD LVCMOS33 [get_ports {led[1]}]
 set_property PACKAGE_PIN U19 [get_ports {led[2]}]
 	set_property IOSTANDARD LVCMOS33 [get_ports {led[2]}]
-set_property PACKAGE_PIN V19 [get_ports {led[3]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {led[3]}]
-set_property PACKAGE_PIN W18 [get_ports {led[4]}]
-	set_property IOSTANDARD LVCMOS33 [get_ports {led[4]}]
 
 ## Configuration options, can be used for all designs
 set_property CONFIG_VOLTAGE 3.3 [current_design]
