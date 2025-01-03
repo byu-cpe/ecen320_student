92e8b2a6b9720cd0b76687e1f510a2868951367f
Mike Wirthlin <wirthlin@byu.edu>
2025-01-03 11:24:36 -0700

    Merge

--- lab02/lab02_passoff.py
+++ lab02/lab02_passoff.py
@@ -13,15 +13,11 @@ import test_suite_320
 import repo_test
 
 def main():
-    tester = test_suite_320.build_test_suite_320("lab02", max_repo_files = 15)
-    # Add a test that checks to make sure the vivado tools are available in the executing environment
-    #  (check_exec_test)
-    # tester.add_make_test("about")
-    # tester.add_make_test("reverse")
-    # tester.add_make_test("upper")
-    # tester.add_make_test("lineno")
-    # tester.add_make_test("bottom")
-    # tester.add_file_checks(["reverse.txt", "upper.txt", "upper.txt", "lineno.txt", "bottom.txt"])
+    # Check on vivado
+    tester = test_suite_320.build_test_suite_320("lab02", start_date="01/20/2025")
+    tester.add_Makefile_rule("sim_adder", ["sim_adder.tcl"], ["sim_adder.log"])
+    tester.add_Makefile_rule("synth_adder", ["synth_adder.tcl"], ["synth_adder.log", "binary_adder.dcp"])
+    tester.add_Makefile_rule("implement_adder", ["implement_adder.tcl"], ["implement_adder.log", "binary_adder.dcp", "binary_adder.bit"])
     tester.run_tests()
 
 if __name__ == "__main__":
